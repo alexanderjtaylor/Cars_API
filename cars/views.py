b6815900ad92fbdecd334052dfa17c37603ecd26
Alex <alexandertaylor315@gmail.com>
2022-10-16 00:58:03 -0400

    added post option to existing function in views.py

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,22 @@
+from urllib import request
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
 
 
-@api_view(['GET'])
-def cars_list(resquest):
+@api_view(['GET', 'POST'])
+def cars_list(request):
     
-    return Response('ok')
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = CarSerializer(data = request.data)
+        if serializer.is_valid == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
